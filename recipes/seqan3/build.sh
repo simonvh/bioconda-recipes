0dd727e0d0dc32007486db8b1e8721f235841d56
John Lees <lees.john6@gmail.com>
2019-07-08 15:37:23 -0400

    Comments from marehr review

--- recipes/seqan3/build.sh
+++ recipes/seqan3/build.sh
@@ -2,9 +2,5 @@
 
 install -d $PREFIX/include
 install -d $PREFIX/share/cmake
-install -d $PREFIX/share/doc
-install -d  $PREFIX/share/pkgconfig
-install include/seqan $PREFIX/include
-install share/cmake/seqan $PREFIX/share/cmake
-install share/doc/seqan $PREFIX/share/doc
-install share/pkgconfig/* $PREFIX/share/pkgconfig
+install include/seqan3 $PREFIX/include
+install build_system/seqan3-config.cmake $PREFIX/share/cmake
