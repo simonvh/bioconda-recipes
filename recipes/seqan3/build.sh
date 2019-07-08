35cdac6b5f4b0a1fe02841bb4a117a04c5d7f559
John Lees <lees.john6@gmail.com>
2019-07-08 15:46:32 -0400

    Use third form of install

--- recipes/seqan3/build.sh
+++ recipes/seqan3/build.sh
@@ -2,5 +2,5 @@
 
 install -d $PREFIX/include
 install -d $PREFIX/share/cmake
-install include/seqan3 $PREFIX/include
-install build_system/seqan3-config.cmake $PREFIX/share/cmake
+install -t include/seqan3 $PREFIX/include
+install -t build_system/seqan3-config.cmake $PREFIX/share/cmake
